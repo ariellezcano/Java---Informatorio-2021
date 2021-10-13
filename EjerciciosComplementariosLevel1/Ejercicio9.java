1a792cd955d65ade8bf78fdaa844ac982942705e
ariellezcano <ariellezcano1995@gmail.com>
2021-10-13 13:57:25 -0300

    ....

--- EjerciciosComplementariosLevel1/Ejercicio9.java
+++ EjerciciosComplementariosLevel1/Ejercicio9.java
@@ -5,8 +5,24 @@ import java.util.Scanner;
 public class Ejercicio9 {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
     
+    String texto;
+
+    texto = scanner.next();
+
+    int totalCharacters = 0;
+    char temp;
+
+    for (int i = 0; i < texto.length(); i++) {
+
+        temp = exampleString.charAt(i);
+        if (temp == 'i')
+            totalCharacters++;
+    }
+
+    System.out.println("i appears " + totalCharacters + " times in exampleString");
+}
+
         
 
 
