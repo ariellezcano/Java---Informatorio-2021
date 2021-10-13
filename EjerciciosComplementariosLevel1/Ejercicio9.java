2b8c56a62761d1e77c8b477d85797500ce860ddc
ariellezcano <ariellezcano1995@gmail.com>
2021-10-13 13:56:55 -0300

    falta

---
+++ EjerciciosComplementariosLevel1/Ejercicio9.java
@@ -0,0 +1,16 @@
+package EjerciciosComplementariosLevel1;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    
+        
+
+
+    }
+}
+//Dado un String de entrada (frase, texto, etc) 
+//calcular la cantidad de veces que aparece una letra dada.
