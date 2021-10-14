215fc7f5d7f84a64e9676c2750bd828fc01714ef
ariellezcano <ariellezcano1995@gmail.com>
2021-10-14 09:49:08 -0300

    ejercicio 9

--- EjerciciosComplementariosLevel1/Ejercicio9.java
+++ EjerciciosComplementariosLevel1/Ejercicio9.java
@@ -7,26 +7,29 @@ public class Ejercicio9 {
     Scanner scanner = new Scanner(System.in);
     
     String texto;
+    int totalCaracteres = 0;
+    char letra;
 
-    texto = scanner.next();
 
-    int totalCharacters = 0;
-    char temp;
+    System.out.println("Ingrese texto:");
+    texto = scanner.nextLine();
+    
+    System.out.println("Ingrese letra a buscar:");
+    letra = scanner.next().charAt(0);
+    scanner.close();
 
     for (int i = 0; i < texto.length(); i++) {
+        char indice = texto.charAt(i);
 
-        temp = exampleString.charAt(i);
-        if (temp == 'i')
-            totalCharacters++;
+        if (letra == indice){
+            totalCaracteres++;
+        } 
     }
-
-    System.out.println("i appears " + totalCharacters + " times in exampleString");
-}
-
-        
-
-
+    
+   if (totalCaracteres == 0){
+       System.out.println("La letra"+" "+ letra +" "+"no existe dentro del texto");
+   }else{
+    System.out.println("la cantidad de "+ letra +" en el sistema es:"+" "+totalCaracteres);
+   }
     }
 }
-//Dado un String de entrada (frase, texto, etc) 
-//calcular la cantidad de veces que aparece una letra dada.
