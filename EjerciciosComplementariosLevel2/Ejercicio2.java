93e5aaf6f11b40cb74816fcd1c359afa43f202f3
ariellezcano <ariellezcano1995@gmail.com>
2021-10-15 14:12:46 -0300

    modificar 2

---
+++ EjerciciosComplementariosLevel2/Ejercicio2.java
@@ -0,0 +1,48 @@
+package EjerciciosComplementariosLevel2;
+//Crear un ArrayList, agregar 5 números enteros.
+// Luego, agregar un número entero al principio de la lista y otro al final.
+// Por último, iterar e imprimir los elementos de la lista
+// y el tamaño de la misma (antes y después de agregar a en la primera y última posición).
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> listnumeros = new ArrayList<>();
+        
+        int numeros;
+        System.out.println("************************");
+        System.out.println("Ingrese 5 números enteros");
+        
+        for (int i = 0; i < 5; i++) {
+            numeros = scanner.nextInt();
+            listnumeros.add(numeros);
+        }
+        
+        System.out.println("Números agregados");
+        for (int i = 0; i < listnumeros.size(); i++) {
+            System.out.println(i+1+" "+""+listnumeros.get(i));
+        }
+
+        if (listnumeros.size()!=0){
+            System.out.println("Agregar elemento al principio");
+            numeros = scanner.nextInt();
+            listnumeros.add(0, numeros);
+            System.out.println("Agregar elemento al final");
+            numeros = scanner.nextInt();
+            listnumeros.add(-1, numeros);
+        }
+
+        System.out.println("Lista actualizada");
+        System.out.println("Tamaño de la lista"+" "+listnumeros.size());
+        for (int i = 0; i < listnumeros.size(); i++) {
+            System.out.println(i+1+" "+""+listnumeros.get(i));
+        }
+
+    }
+  
+}
