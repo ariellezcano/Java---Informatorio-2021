61f1341f2c175d3c930b40a14bc484e4f184ecc0
ariellezcano <ariellezcano1995@gmail.com>
2021-10-15 10:49:35 -0300

    ejercicio 1

--- EjerciciosComplementariosLevel2/Ejercicio1.java
+++ EjerciciosComplementariosLevel2/Ejercicio1.java
@@ -9,53 +9,33 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         List<String> ciudades = new ArrayList<String>();
-        System.out.println("QUE DESEA REALIZAR:");
-        System.out.println("1 = AGREGAR CIUDADES / 2 = VER RANKING");
-
-        int valor = input.nextInt();
         String ciudad;
-        switch (valor) {
-            case 1:{  
-                    System.out.println("Ingrese nombre de la ciudad");
-                    input.nextLine();
-                    ciudad = input.nextLine();
-                    System.out.println("************************");
-                    ciudades.add(ciudad);
-                while(true){
-                    System.out.println("Desea seguir agregando ciudades? 1=si / 2=no");
-                    int pregunta = input.nextInt();
-                    int respuesta = pregunta;
-                    if (respuesta == 1) {
-                        System.out.println("Ingrese nombre de la ciudad");
-                        input.nextLine();
-                        ciudad = input.nextLine();
-                        System.out.println("************************");
-                        ciudades.add(ciudad);
-                    } else {
-                        System.out.println("************************");
-                        System.out.println("Carga finalizada");        
-                    }
-                    break;
-                }
-                
-            }
-                
-                //
-            case 2:{
-                System.out.println("Mostrar elementos");
+
+        System.out.println("Ingrese nombre de la ciudad");
+        ciudad = input.nextLine();
+        System.out.println("************************");
+        ciudades.add(ciudad);
+        while(true){
+            System.out.println("Desea seguir agregando ciudades? 1=si / 2=no");
+            int resultado = input.nextInt();
+            input.nextLine();
+            if (resultado == 1) {
+                System.out.println("Ingrese nombre de la ciudad");
+                ciudad = input.nextLine();
+                System.out.println("************************");
+                ciudades.add(ciudad);
+            } else {
+                System.out.println("************************");
+                System.out.println("Carga finalizada");
+                System.out.println("************************");
+                System.out.println("Ranking");
                 for (int i = 0; i < ciudades.size(); i++) {
-                    System.out.println("1"+ ciudades.get(0));
-                    System.out.println("2"+ ciudades.get(0));
-                    System.out.println("3"+ ciudades.get(0));
+                    System.out.println(i+1 +" "+ ciudades.get(i));
                 }
-                
+                break;            
             }
-                break;
-            default:
-                System.out.println("DEBE SELECCIONAR UN ELEMENTO");
-                break;
- 
-            }       
+                    
+        }       
     
     }
 }
